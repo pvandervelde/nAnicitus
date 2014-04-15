edd5598a3523d589d2a7058ec553b6c4759778bf
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-04-16 09:44:36 +1200

    Marking source files as read-only.
    references #6

--- src/Nanicitus.Core/SymbolIndexer.cs
+++ src/Nanicitus.Core/SymbolIndexer.cs
@@ -702,6 +702,7 @@ namespace Nanicitus.Core
                 }
 
                 File.Copy(sourceFile, destinationFile);
+                File.SetAttributes(destinationFile, FileAttributes.ReadOnly);
 
                 m_Diagnostics.Log(
                     LevelToLog.Info,
